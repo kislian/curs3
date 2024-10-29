d202483f11f22e5eb17213aa1ad584fd6f041214
kislian <kisli_an@mail.ru>
2024-10-29 21:45:08 +0800

    Curs3

---
+++ src/main/java/pro/sky/SpringBootTelegram/SpringBootTelegramApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.SpringBootTelegram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTelegramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTelegramApplication.class, args);
+	}
+
+}
